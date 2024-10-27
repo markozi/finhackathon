8aed5cda79fadb4e5f4c9bdf05a42bf51ef67de2
Marko <marko@Markos-MacBook-Pro.local>
2024-10-27 17:23:32 +0100

    Can compute balances

--- backend/backend.tests/UnitTest1.cs
+++ backend/backend.tests/UnitTest1.cs
@@ -1,10 +1,91 @@
+using FluentAssertions;
+
 namespace backend.tests;
 
-public class UnitTest1
+public class BalanceTest
 {
     [Fact]
-    public void Test1()
+    public void Balance_0_Debit_100_NewBalance_Minus100()
     {
+        // Arrange
+        var account = Account();
+        var originalAmount = new Amount(0);
+        var debitAmount = new Amount(100);
+        
+        var originalBalance = Balance.Reconstitute(account, DateTime.Today, originalAmount);
+        
+        // Act
+        var newBalance = originalBalance.Debit(debitAmount);
+        
+        // Assert
+        var expectedBalance = -100;
+        newBalance.Amount.Value.Should().Be(expectedBalance);
+        newBalance.Account.Should().Be(account);
+        newBalance.Timestamp.Should().BeCloseTo(DateTime.Now, TimeSpan.FromSeconds(10));
+    }
+    
+    [Fact]
+    public void Balance_0_Credit_100_NewBalance_100()
+    {
+        // Arrange
+        var account = Account();
+        var originalAmount = new Amount(0);
+        var debitAmount = new Amount(100);
+        
+        var originalBalance = Balance.Reconstitute(account, DateTime.Today, originalAmount);
+        
+        // Act
+        var newBalance = originalBalance.Credit(debitAmount);
+        
+        // Assert
+        var expectedBalance = 100;
+        newBalance.Amount.Value.Should().Be(expectedBalance);
+        newBalance.Account.Should().Be(account);
+        newBalance.Timestamp.Should().BeCloseTo(DateTime.Now, TimeSpan.FromSeconds(10));
+    }
+    
+    [Fact]
+    public void Balance_100_Debit_100_NewBalance_0()
+    {
+        // Arrange
+        var account = Account();
+        var originalAmount = new Amount(100);
+        var debitAmount = new Amount(100);
+        
+        var originalBalance = Balance.Reconstitute(account, DateTime.Today, originalAmount);
+        
+        // Act
+        var newBalance = originalBalance.Debit(debitAmount);
+        
+        // Assert
+        var expectedBalance = 0;
+        newBalance.Amount.Value.Should().Be(expectedBalance);
+        newBalance.Account.Should().Be(account);
+        newBalance.Timestamp.Should().BeCloseTo(DateTime.Now, TimeSpan.FromSeconds(10));
+    }
+    
+    [Fact]
+    public void Balance_100_Credit_100_NewBalance_200()
+    {
+        // Arrange
+        var account = Account();
+        var originalAmount = new Amount(100);
+        var debitAmount = new Amount(100);
+        
+        var originalBalance = Balance.Reconstitute(account, DateTime.Today, originalAmount);
+        
+        // Act
+        var newBalance = originalBalance.Credit(debitAmount);
+        
+        // Assert
+        var expectedBalance = 200;
+        newBalance.Amount.Value.Should().Be(expectedBalance);
+        newBalance.Account.Should().Be(account);
+        newBalance.Timestamp.Should().BeCloseTo(DateTime.Now, TimeSpan.FromSeconds(10));
+    }
 
+    private static AccountReference Account()
+    {
+        return new AccountReference(Guid.NewGuid());
     }
 }
